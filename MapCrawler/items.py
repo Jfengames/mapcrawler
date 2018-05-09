e410764513017b415aefa44b306e723c94884d80
henry <jiahuiwl@chinamobile.com>
2018-05-09 17:52:23 +0800

    一天写一点

--- MapCrawler/items.py
+++ MapCrawler/items.py
@@ -11,4 +11,8 @@ import scrapy
 class MapcrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    province  = scrapy.Field() #省份
+    city = scrapy.Field() #城市
+    resident = scrapy.Field() #居民区
+    building = scrapy.Field() #建筑
     pass
--- MapCrawler/spiders/gaode_crawler.py
+++ MapCrawler/spiders/gaode_crawler.py
@@ -12,6 +12,18 @@ class GaodeCrawler(scrapy.Spider):
         先爬取1km×1km的方格
         :return:
         """
+        jinshui_adcode='410105' #金水区的adcode
+        start_long_lat = '34.808881,113.652670'
+        resolution = 0.01 # 直接加到经纬度上，大概1km
+
         pass
+
+
+    def parse(self,response):
+        """
+        解析response，并保存需要的信息到items
+        :param response:
+        :return:
+        """
 
 
