52f89f2119653e202688f4258dfdad50a808a8f4
henry <jiahuiwl@chinamobile.com>
2018-05-11 10:03:20 +0800

    添加坐标系转换工具

---
+++ MapCrawler/toolskit.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# coding =urt-8
+
+
+"""
+坐标系转换……
+
+"""
+
+def gcj2wgs(long,lat):
+    assert type(long)==float,'经度数据类型需要为浮点型 '
+    assert type(lat)==float,'维度数据类型需要为浮点型 '
+    a = 6378245.0  # 克拉索夫斯基椭球参数长半轴a
+    ee = 0.00669342162296594323  # 克拉索夫斯基椭球参数第一偏心率平方
+    PI = 3.14159265358979324  # 圆周率
+    # 以下为转换公式
+    x = long - 105.0
+    y = lat - 35.0
+    # 经度
+    dLon = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * math.sqrt(abs(x));
+    dLon += (20.0 * math.sin(6.0 * x * PI) + 20.0 * math.sin(2.0 * x * PI)) * 2.0 / 3.0;
+    dLon += (20.0 * math.sin(x * PI) + 40.0 * math.sin(x / 3.0 * PI)) * 2.0 / 3.0;
+    dLon += (150.0 * math.sin(x / 12.0 * PI) + 300.0 * math.sin(x / 30.0 * PI)) * 2.0 / 3.0;
+    # 纬度
+    dLat = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * math.sqrt(abs(x));
+    dLat += (20.0 * math.sin(6.0 * x * PI) + 20.0 * math.sin(2.0 * x * PI)) * 2.0 / 3.0;
+    dLat += (20.0 * math.sin(y * PI) + 40.0 * math.sin(y / 3.0 * PI)) * 2.0 / 3.0;
+    dLat += (160.0 * math.sin(y / 12.0 * PI) + 320 * math.sin(y * PI / 30.0)) * 2.0 / 3.0;
+    radLat = lat / 180.0 * PI
+    magic = math.sin(radLat)
+    magic = 1 - ee * magic * magic
+    sqrtMagic = math.sqrt(magic)
+    dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * PI);
+    dLon = (dLon * 180.0) / (a / sqrtMagic * math.cos(radLat) * PI);
+    _long = long - dLon
+    _lat = lat - dLat
+
+    return _long,_lat
+
+def wgc2gcj(long,lat):
+    assert type(long)==float,'经度数据类型需要为浮点型 '
+    assert type(lat)==float,'维度数据类型需要为浮点型 '
+    pass
+
+    return _long,_lat
